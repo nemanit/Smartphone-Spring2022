322153bb06209b2de44feedbbc8d2c307b134577
nemanit <77519029+nemanit@users.noreply.github.com>
2022-03-27 01:22:44 -0700

    practice

---
+++ Practice/Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Tejaswi Nemani on 3/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
